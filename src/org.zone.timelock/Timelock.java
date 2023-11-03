b3d5484fb7a0d2d75412824b6a9d0494e6237f8c
vincenzoiovino <41876613+vincenzoiovino@users.noreply.github.com>
2023-11-03 23:59:02 +0100

    live with timelock.zone api

--- src/org.zone.timelock/Timelock.java
+++ src/org.zone.timelock/Timelock.java
@@ -1,12 +1,19 @@
 package org.zone.timelock;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.regex.Pattern;
+
+import javax.swing.JOptionPane;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -46,32 +53,48 @@ public class Timelock {
 		}
 		return content.toString();
 	}
-*/	
+	 */	
 	private static String getPublicKeyfromURL(long Round, String scheme) throws IOException // TODO: handle exceptions in a nice way
 	{
-		JSONObject jsonObj;
-		 JSONObject finalobj;
+		JSONObject jsonObj=null;
+		JSONObject finalobj=null;
 		try
 		{
 			int scheme_numeric;
 			if (scheme.equals("secp256k1")) scheme_numeric=2;
 			else scheme_numeric=-1;
-			URL url = new URL("https://api.timelock.zone/tlcs/timelock/v1beta1/keypairs/round_and_scheme/"+Round+"/"+scheme_numeric);
+			URL url = new URL("https://demo.timelock.zone/tlcs/timelock/v1beta1/keypairs/round_and_scheme/"+Round+"/"+scheme_numeric);
 			JSONParser parser = new JSONParser();
-	        jsonObj = (JSONObject) parser.parse(new InputStreamReader(url.openStream()));
-	        JSONArray jsonArray = (JSONArray) jsonObj.get("keypairs");
-	        finalobj= (JSONObject) jsonArray.get(0);
-	        String s=(String) finalobj.get("public_key_pem"); 
+			jsonObj = (JSONObject) parser.parse(new InputStreamReader(url.openStream()));
+			JSONArray jsonArray = (JSONArray) jsonObj.get("keypairs");
+			finalobj= (JSONObject) jsonArray.get(0);
+			String s=(String) finalobj.get("public_key_pem"); 
+			/*
+			System.out.println(jsonObj.toJSONString());
+			System.out.println(finalobj.toJSONString());
+			Path logfile=Paths.get("log.txt");
+		    Files.write(logfile, finalobj.toJSONString().getBytes());
+		    Path logfile2=Paths.get("log2.txt");
+		    Files.write(logfile2, (Round+"").getBytes());
+			 */
 
 			if (s.equals("")) throw new IOException();
 			else return s;
-	        
+
 		}
 		catch(Exception e)
 		{
+			e.printStackTrace();
+			/*
+			File file = new File("log.txt");
+			PrintStream ps = new PrintStream(file);
+			e.printStackTrace(ps);
+			Path logfile=Paths.get("log2.txt");
+		    Files.write(logfile, (Round+"").getBytes());
+			 */
 			throw new IOException();
 		}
-		
+
 	}
 
 
@@ -81,27 +104,33 @@ public class Timelock {
 		JSONObject finalobj;
 		try
 		{
-			URL url = new URL("https://api.timelock.zone/tlcs/timelock/v1beta1/keypairs/round_and_scheme/"+Round+"/"+scheme_numeric); 
+			int scheme_numeric;
+			if (scheme.equals("secp256k1")) scheme_numeric=2;
+			else scheme_numeric=-1;
+			URL url = new URL("https://demo.timelock.zone/tlcs/timelock/v1beta1/keypairs/round_and_scheme/"+Round+"/"+scheme_numeric); 
 			JSONParser parser = new JSONParser();
-	        jsonObj = (JSONObject) parser.parse(new InputStreamReader(url.openStream()));
-	        JSONArray jsonArray = (JSONArray) jsonObj.get("keypairs");
-	        finalobj= (JSONObject) jsonArray.get(0);
-	        String s=(String) finalobj.get("private_key_pkcs8");
+			jsonObj = (JSONObject) parser.parse(new InputStreamReader(url.openStream()));
+			JSONArray jsonArray = (JSONArray) jsonObj.get("keypairs");
+			finalobj= (JSONObject) jsonArray.get(0);
+			String s=(String) finalobj.get("private_key_pkcs8");
 
 			if (s.equals("")) throw new IOException();
 			else return s;
-	        
+
 		}
 		catch(Exception e)
 		{
+			e.printStackTrace();
 			throw new IOException();
 		}
-		
+
 	}
 
-	
 
-	private static long DRAND_GENESIS_TIME=1677685200;
+
+	// private static long DRAND_GENESIS_TIME=1677685200;
+	private static long DRAND_GENESIS_TIME=1692803367;
+
 	private static int DRAND_FREQUENCY=3;
 	private static byte[] stripPEM(String pem) throws IOException {
 
@@ -113,64 +142,66 @@ public class Timelock {
 
 	public static long DateToRound(Date date){
 		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.HOUR_OF_DAY,0);
+		cal.setTime(date);
+		cal.set(Calendar.HOUR_OF_DAY,1);
 		cal.set(Calendar.MINUTE,0);
 		cal.set(Calendar.SECOND,0);
-		date = cal.getTime();
-		long t= ((date.getTime())/1000);
+		Date newdate = cal.getTime();
+		long t= ((newdate.getTime())/1000);
 		return (t-Timelock.DRAND_GENESIS_TIME)/Timelock.DRAND_FREQUENCY;
 	}
 	public static byte[] getPublicKeyFromRound(long Round, String Scheme) throws IOException {
 		// TODO: retrieve pk for given round Round and scheme Scheme
 		// for the moment the pk is embedded
-		
-/*		
-        try{
-        return stripPEM(getPublicKeyfromURL(Round,Scheme));
 
-} catch(IOException e) {
-throw  new IOException();
-}
-		
-        
+
+		try{
+			return stripPEM(getPublicKeyfromURL(Round,Scheme));
+
+		} catch(IOException e) {
+			e.printStackTrace();
+			throw  new IOException();
+		}
+
+		/*        
 
 		String pkpem="-----BEGIN PUBLIC KEY-----\n" +
 				"MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEOErqrRCc3yBTCdQNfUQNM85JJHXOqYSH\n" +
 				"ibnuF1AtHTgc1iOxS/OlGyVctEF+wJMLrvc/nrd2GhRYcqtsJu9Gfw==\n" +
 				"-----END PUBLIC KEY-----\n";
-*/
-		
+
+
 		String pkpem="-----BEGIN PUBLIC KEY-----\n"
 				+ "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAE4ayNtqjWab3VdTiCozknOoZOETJoFWRU\n"
 				+ "8BB8JdCsns3x5G9jsrkw4iJueGOwVQ+w1cNBqOlRWTVBkNK2Kjs9bQ==\n"
 				+ "-----END PUBLIC KEY-----\n";
 		return stripPEM(pkpem);
+		 */
 
 	}
 	public static byte[] getSecretKeyFromRound(long Round, String Scheme) throws IOException {
 
 		// TODO: retrieve sk for given round Round and given scheme Scheme
 		// for the moment the sk is embedded
+		try{
+			return stripPEM(getSecretKeyfromURL(Round,Scheme));
+
+		} catch(IOException e) {
+			throw  new IOException();
+		}
 		/*
-        try{
-        return stripPEM(getSecretKeyfromURL(Round,Scheme));
-
-} catch(IOException e) {
-throw  new IOException();
-}
-
-		 
 		String skpem="-----BEGIN PRIVATE KEY-----\n" +
 				"MIGEAgEAMBAGByqGSM49AgEGBSuBBAAKBG0wawIBAQQg+uEhcA+bG/44RS/COUJa\n" +
 				"bjwVrYcMKN8zby1LowdBvnihRANCAAQ4SuqtEJzfIFMJ1A19RA0zzkkkdc6phIeJ\n" +
 				"ue4XUC0dOBzWI7FL86UbJVy0QX7Akwuu9z+et3YaFFhyq2wm70Z/\n" +
 				"-----END PRIVATE KEY-----\n";
-		*/
 		String skpem="-----BEGIN PRIVATE KEY-----\n"
 				+ "MD4CAQAwEAYHKoZIzj0CAQYFK4EEAAoEJzAlAgEBBCCcj8jXC0N8VUW3GWHbv946\n"
 				+ "P1n1MSnnuHL+oui/xp77xw==\n"
 				+ "-----END PRIVATE KEY-----\n";
+
 		return stripPEM(skpem);
+		 */
 
 	}
 }
